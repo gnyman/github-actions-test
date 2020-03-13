4c8936ceb047c50f3885c72a6059e4b76832ada7
Gabriel Nyman <gabriel@nyman.re>
2020-03-13 12:56:51 +0200

    Try with a package

--- main.go
+++ main.go
@@ -1,29 +1,109 @@
+// Copyright 2015 The TCell Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use file except in compliance with the License.
+// You may obtain a copy of the license at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// boxes just displays random colored boxes on your terminal screen.
+// Press ESC to exit the program.
 package main
 
 import (
 	"fmt"
-	"io"
+	"math/rand"
 	"os"
+	"time"
+
+	"github.com/gdamore/tcell"
 )
 
-func main() {
-	fmt.Println("hello world")
+func makebox(s tcell.Screen) {
+	w, h := s.Size()
 
-	f, err := os.Open("art.ansi")
-	if err != nil {
-		panic(err)
+	if w == 0 || h == 0 {
+		return
 	}
 
-	buffer := make([]byte, 1)
-	for {
-		_, err := f.Read(buffer)
+	glyphs := []rune{'@', '#', '&', '*', '=', '%', 'Z', 'A'}
 
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println(err)
-			}
-			break
+	lx := rand.Int() % w
+	ly := rand.Int() % h
+	lw := rand.Int() % (w - lx)
+	lh := rand.Int() % (h - ly)
+	st := tcell.StyleDefault
+	gl := ' '
+	if s.Colors() > 256 {
+		rgb := tcell.NewHexColor(int32(rand.Int() & 0xffffff))
+		st = st.Background(rgb)
+	} else if s.Colors() > 1 {
+		st = st.Background(tcell.Color(rand.Int() % s.Colors()))
+	} else {
+		st = st.Reverse(rand.Int()%2 == 0)
+		gl = glyphs[rand.Int()%len(glyphs)]
+	}
+
+	for row := 0; row < lh; row++ {
+		for col := 0; col < lw; col++ {
+			s.SetCell(lx+col, ly+row, st, gl)
 		}
-		os.Stdout.Write(buffer)
 	}
+	s.Show()
+}
+
+func main() {
+
+	tcell.SetEncodingFallback(tcell.EncodingFallbackASCII)
+	s, e := tcell.NewScreen()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+	if e = s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+	s.SetStyle(tcell.StyleDefault.
+		Foreground(tcell.ColorBlack).
+		Background(tcell.ColorWhite))
+	s.Clear()
+
+	quit := make(chan struct{})
+	go func() {
+		for {
+			ev := s.PollEvent()
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				switch ev.Key() {
+				case tcell.KeyEscape, tcell.KeyEnter:
+					close(quit)
+					return
+				case tcell.KeyCtrlL:
+					s.Sync()
+				}
+			case *tcell.EventResize:
+				s.Sync()
+			}
+		}
+	}()
+
+	cnt := 0
+	dur := time.Duration(0)
+	for i := 0; i < 10; i++ {
+		start := time.Now()
+		makebox(s)
+		cnt++
+		dur += time.Now().Sub(start)
+	}
+
+	fmt.Printf("Finished %d boxes in %s\n", cnt, dur)
+	fmt.Printf("Average is %0.3f ms / box\n", (float64(dur)/float64(cnt))/1000000.0)
 }
