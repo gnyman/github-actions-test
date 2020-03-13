8a26ba479adf2a55e832c911e9098d844f397382
Gabriel Nyman <gabriel@nyman.re>
2020-03-13 12:42:39 +0200

    Display some ANSI

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	f, err := os.Open("art.ansi")
+	if err != nil {
+		panic(err)
+	}
+
+	buffer := make([]byte, 1)
+	for {
+		_, err := f.Read(buffer)
+
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+			break
+		}
+		os.Stdout.Write(buffer)
+	}
 }
